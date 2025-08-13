ee82b608d44671c9eebf316a31b6ce6907d35e9a
darkwolfjazz <1906349@kiit.ac.in>
2025-08-13 12:05:28 +0530

    deployment fix error 404

--- project-girlfriend-frontend/src/api.js
+++ project-girlfriend-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const prodUrl=axios.create({
-    baseURL:import.meta.env.VITE_APP_API_PROD_URL
+    baseURL: import.meta.env.VITE_API_PROD_URL
 });
 
 export default prodUrl;
